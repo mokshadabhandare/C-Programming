3344b4407f3d7e364dc31650962dfa6fcda765c3
mokshada <mokshadabhandare.01@gmail.com>
2024-02-25 19:26:14 +0530

    c

--- array.c
+++ array.c
@@ -2,7 +2,8 @@
 int main()
 {
     int no=0;
-    printf("Enter number");
+    int sum=0;
+    printf("Enter array size");
     scanf("%d",&no);
 
     int arr[no];
@@ -11,11 +12,18 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
+
     printf("array element\n");
     for(int i=0;i<no;i++)
     {
         printf("%d",arr[i]);
     }
-    for(int i=0;i<arr[0])
+
+    for(int i=0;i<no;i++)
+    {
+        sum=sum+arr[i];
+    }
+           printf("sum of the  array =%d\n",sum);
+
     return 0;
 }
