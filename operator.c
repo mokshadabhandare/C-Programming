c3997db52a25b70657a5a5c423d56d34d32bd520
mokshada <mokshadabhandare.01@gmail.com>
2024-02-28 22:25:30 +0530

    c

---
+++ operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int no=10;
+
+    printf("Value of no:%d\n",no);
+
+    printf("Address of no:%d\n",&no);
+
+    printf("Size of no:%d\n",sizeof(no));
+
+
+    return 0;
+}
