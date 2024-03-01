d06c870e713d45c254a398b0f5206aac21c6c396
mokshada <mokshadabhandare.01@gmail.com>
2024-03-01 22:41:02 +0530

    c

---
+++ structure2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+struct Marvellous
+{
+    int no;
+    float f;
+    int i;
+};
+
+
+int main()
+{
+    struct Marvellous mobj;
+
+    struct Marvellous *ptr =&mobj;
+
+    ptr->no=11;
+    ptr->f=90.99;
+    ptr->i=21;
+
+    printf("%d\n",ptr->no);
+    printf("%d\n",mobj.no);
+    return 0;
+}
