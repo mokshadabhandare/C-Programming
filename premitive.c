22dadb657bfc844b47fc237fed7eb2246cde3496
mokshada <mokshadabhandare.01@gmail.com>
2024-03-07 21:40:03 +0530

    c

---
+++ premitive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("Inside main function..\n");
+
+    char cValue ='M';
+    int iValue =11;
+    float fValue =90.98f;
+    double dValue=90.67452378;
+
+    
+    return 0;
+}
