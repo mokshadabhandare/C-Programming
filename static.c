1c1eead2f02a4146d68e3eb4efecdb8046c39a0d
mokshada <mokshadabhandare.01@gmail.com>
2024-03-07 22:31:39 +0530

    c

---
+++ static.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void Marvellous()
+{
+    int i=11;
+    i++;
+    printf("Value of x is :%d\n",x);
+}
+int main()
+{
+    printf("Demonstration of auto storage class..\n");
+    
+     Marvellous();
+        Marvellous();
+            Marvellous();
+
+    return 0;
+}
