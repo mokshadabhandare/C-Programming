78ef157374d9912afed33475bb458449b8fc8fe7
mokshada <mokshadabhandare.01@gmail.com>
2024-03-20 22:47:05 +0530

    c++

--- OOP1.cpp
+++ OOP1.cpp
@@ -11,8 +11,10 @@ class Demo
     Demo() //Constructor
     {
         cout<<"Inside Constructor\n";
+        no1=11;
+        no2=21;
     }
-    Demo() //Destructor
+    ~Demo() //Destructor
     {
         cout<<"Inside Destructor\n";
     }
@@ -25,6 +27,11 @@ class Demo
 
 int main()
 {
-
+    cout<<"Inside main\n";
+   Demo obj1;
+   cout<<"Size of object is:" <<sizeof (obj1)<<"\n";
+   cout<<"Value of no1:"<<obj1.no1<<"\n";
+   cout<<"Value of no2:"<<obj1.no2<<"\n";
+   obj1.Fun();
     return 0;
 }
