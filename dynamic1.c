729d50e15a0cb3a5f7925318e932df7af8a17f81
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-04 20:31:27 +0530

    c

--- dynamic1.c
+++ dynamic1.c
@@ -5,9 +5,12 @@ int main()
 {
     int Arr[5];
     int Size=0;
+    int *ptr=NULL;
 
     printf("Enter number of elements:\n");
     scanf("%d",&Size);
+
+    ptr=malloc(Size * sizeof(int));
 
     return 0;
 }
