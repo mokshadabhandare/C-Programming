32fb17c198636599cf27ffa9c7550935ccce9360
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-11 22:38:58 +0530

    java

---
+++ OOP.java
@@ -0,0 +1,36 @@
+class OOP
+{
+    public static void main (String Arg[])
+    {
+
+    }
+}
+
+class Arithematic
+{
+    public int No1;
+    public int No2;
+
+    public Arithematic()
+    {
+        this.No1=0;
+        this.No2=0;
+    }
+    public Arithematic(int A,int B)
+    {
+        this.No1=A;
+        this.No2=B;
+    }
+    public Addition()
+    {
+        int Ans=0;
+        Ans=No1+No2;
+        return Ans;
+    }
+     public Substraction()
+    {
+        int Ans=0;
+        Ans=No1-No2;
+        return Ans;
+    }
+}
