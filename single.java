e7d2ca732c41299177ab22ca47c674282d96500b
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-17 19:48:09 +0530

    java

---
+++ single.java
@@ -0,0 +1,33 @@
+class Base
+{
+    public int A,B;
+    public Base()
+    {
+        System.out.println("Base constructor");
+    }
+    void fun()
+    {
+        System.out.println("Inside Base Fun");
+    }
+}
+
+class Derived extends Base
+{
+    public int X,Y;
+    Public Derived()
+    {
+        System.out.println("Derived constructor");
+    }
+    void Gun()
+    {
+        System.out.println("Inside derived gun");
+    }
+}
+
+class single
+{
+    public static void main(String Arg[])
+    {
+
+    }
+}
