c26ab79496588ac0df7f8f1473b2232dbe54338d
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-19 19:08:05 +0530

    java

---
+++ final1.java
@@ -0,0 +1,18 @@
+class Demo{
+    public int No1;
+    public final int No2=21;
+
+    public Demo()
+    {
+        No1=11;
+    }
+}
+
+class final1 
+{
+    public static void main(String Arg[])
+    {
+       Demo obj=new Demo();
+       
+    }
+}
