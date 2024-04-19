f66fe167bc4e852270fa6928517c0f156031d717
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-19 19:43:11 +0530

    java

--- final2.java
+++ final2.java
@@ -1,4 +1,26 @@
+class Base
+{
+    public void fun()
+    {
+        System.out.println("Inside base fun");
+    }
+    public final void gun()
+    {
+        System.out.println("Inside base Gun");
+    }
+}
 
+class Derived extends Base
+{
+public void fun()
+    {
+        System.out.println("Inside derived fun");
+    }
+    public void gun()
+    {
+        System.out.println("Inside derived Gun");
+    }
+}
 
 class final2
 {
