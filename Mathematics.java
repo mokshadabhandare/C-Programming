472abd39ecb964168df004e4ef3b9e8a1fd9bf20
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-19 20:29:49 +0530

    java

---
+++ Mathematics.java
@@ -0,0 +1,13 @@
+package Infosystems;
+
+public class Mathematics
+{
+    public int Multiplication(int A,int B)
+    {
+        return A*B;
+    }
+    public int Divison(int A,int B)
+    {
+        return A/B;
+    }
+}
