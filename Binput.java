8ea521b61a245a237effa7310498d9b72994dba3
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-04-24 19:23:09 +0530

    java

---
+++ Binput.java
@@ -0,0 +1,15 @@
+import java.oi.*;
+
+class Binput
+{
+    public static void main (String Arg[])
+    {
+       InputStreamReader iobj=new InputStreamReader(System.in);
+       BufferedReader bobj=new BufferedReader(iobj);
+
+       System.out.println("Enter yout name:");
+       String name=bobj.readLine()readLine
+       
+       System.out.println("Welcome:"+name);
+    } 
+}
