9ad66201b2ce1cc5200a725614358ce778a5bdb9
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-05-01 17:18:34 +0530

    c

---
+++ CreateFile.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<Stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+    char Name[30];
+    int fd=0;
+
+    printf("Enter name of file that you want to create:\n");
+    scanf("%s",Name);
+
+    fd=creat(Name,0777);
+    if(fd==-1)
+    {
+        printf("Unable to craete file\n");
+    }
+    else
+    {
+        printf("File gets created with FD %d\n",fd);
+    }
+    return 0;
+}
