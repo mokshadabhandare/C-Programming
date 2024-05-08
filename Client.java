5e39d7154a009f52f7fc9a789fe9b851426dccc3
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-05-08 21:27:08 +0530

    java

---
+++ Client.java
@@ -0,0 +1,11 @@
+import java.net.*;
+import java.io.*;
+
+class Client
+{
+    public static void main(String Arg[]) throws Exception
+    {
+       System.out.println("Client application is running..");
+       
+    }
+}
