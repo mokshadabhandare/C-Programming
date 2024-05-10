fa115aded107b3ecb72f4426c19b28aa6044ae92
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-05-10 20:04:10 +0530

    c

--- command.c
+++ command.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-int main()
+int main(int argc,char *argv[])
 {
     int i=0;
+
+    printf("Number of command line%d\n",argc);
+
+    for(i=0;i<argc;i++)
+    {
+        printf("%s\n",argv[i]);
+    }
+    return 0;
 }
